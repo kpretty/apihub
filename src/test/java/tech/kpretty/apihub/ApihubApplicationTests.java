f06e8b81fac31708884810e82a647602a050083e
wjun <wjunjobs@outlook.com>
2023-07-12 10:49:32 +0800

    test: 测试类

---
+++ src/test/java/tech/kpretty/apihub/ApihubApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.kpretty.apihub;
+
+import tech.kpretty.apihub.entity.Query;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+
+@SpringBootTest
+class ApihubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
